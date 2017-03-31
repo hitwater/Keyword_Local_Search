72341bd0b1a0d1453672fb2cfe9f90d87f168094
Guiming <guiming2011@gmail.com>
2017-03-31 15:20:44 -0500

    First commit

---
+++ js/config.js
@@ -0,0 +1,32 @@
+/*
+ * Calaca - Search UI for Elasticsearch
+ * https://github.com/romansanchez/Calaca
+ * http://romansanchez.me
+ * @rooomansanchez
+ * 
+ * v1.2.0
+ * MIT License
+ */
+
+/* Configs */
+/**
+ *
+ * url - Cluster http url
+ * index_name - Index name or comma-separated list
+ * type - Type
+ * size - Number of results to display at a time when pagination is enabled.
+ * search_delay - Delay between actual search request in ms. Reduces number of queries to cluster by not making a request on each keystroke. 
+ */
+/**
+var CALACA_CONFIGS = {
+	url: "http://localhost:9200",
+	index_name: "twitter",
+	type: "tweet",
+	size: 10,
+	search_delay: 500
+}
+
+*/
+var host = "https://6fam5f32gu:tba8fwuj9p@guimings-first-sandb-699906627.us-west-2.bonsaisearch.ne"; //Ex: http://ec2-123-aws.com
+var indexName = "media"; //Ex: twitter
+var docType = "book"; //Ex: tweet
